d71a927a528afdbbb060a6284c1395d9513caf8a
ducddm <ducddm.b8042@st.usth.edu.vn>
2020-01-25 21:45:26 +0700

    ABC gdslg

--- Dog.js
+++ Dog.js
@@ -1,6 +1,14 @@
-function Dog(){
-    Dog.stomach = null;
+var chalk = require('chalk');
+function Dog(name){
+    this.name = name;
+    this.stomach = [];
 }
 function eat(){
     Dog.food = cat;
 }
+
+Dog.prototype.sayHi = function(){
+    console.log("Hi! My name is " + chalk.red(this.name));
+}
+
+module.exports = Dog;
